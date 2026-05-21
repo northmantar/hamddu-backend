1f101887a9786b960c8306078986b6bb0c2e467c
northmantar <northmantar@naver.com>
2026-05-22 00:06:27 +0900

    chore: 어드민 화면 프로세스 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,23 @@
 module.exports = {
-    apps: [
-      {
-        name: 'hamddu-backend',
-        script: 'dist/main.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        env: {
-          // NODE_ENV: 'production',
-          NODE_ENV: 'development',
-        },
+  apps: [
+    {
+      name: 'hamddu-backend',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
       },
-    ],
-  };
+    },
+    {
+      name: 'hamddu-admin-poc',
+      script: 'serve',
+      args: ['poc', '-l', '4000', '--no-clipboard'],
+      interpreter: 'none',
+      autorestart: true,
+      watch: false,
+    },
+  ],
+};
   
